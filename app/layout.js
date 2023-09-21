ef97e4bd01cc301ad688cc980d09a7fc9daec2a8
mohamed younes <bettiouim77@gmail.com>
2023-09-21 17:10:28 +0100

    first commit

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,16 @@ import Footer from '@/components/Footer'
 export const metadata = {
   title: 'JSM Course platform clone',
   description: 'JS MASTERY CLONE BY MOHAMED YOUNES ',
+    other: {
+      'theme-color': '#0d1117',
+      "color-scheme": "dark only",
+      "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+      "twitter:card": "summary_large_image",
+      "og:url": "jsmastery.pro",
+      "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+      "og:type": "website",
+    }
+  
 }
 
 export default function RootLayout({ children }) {
